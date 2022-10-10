c71d4d5a4be23938461ff84ae633aa12af07e368
Isle <90284341+ixiiviixi@users.noreply.github.com>
2022-10-10 16:43:54 -0700

    Comment services templated
    Generalized template of how services is going to be without the bulk logic. That will be tomorrow(oct11).

---
+++ services/commentServices/commentCreateService.js
@@ -0,0 +1,14 @@
+const commentModel = require('../../models/commentsModel.js')
+
+const commentCreateService = async (commentData) => {
+    try {
+
+
+    } catch(error) {
+        throw Error('Error while fetching user. Location: commentCreateService')
+    }
+}
+
+module.exports = {
+    commentCreateService
+}
---
+++ services/commentServices/commentIndexService.js
@@ -0,0 +1,14 @@
+const commentModel = require('../../models/commentsModel.js')
+
+const commentIndexService = async (commentData) => {
+    try {
+
+
+    } catch(error) {
+        throw Error('Error while fetching user. Location: commentIndexService')
+    }
+}
+
+module.exports = {
+    commentIndexService
+}
