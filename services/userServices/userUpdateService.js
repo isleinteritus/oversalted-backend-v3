f19d5e909114445ef80426a769a549a05e09b283
Isle <90284341+ixiiviixi@users.noreply.github.com>
2022-10-14 11:09:10 -0700

    Structure update
    Cleaned up the structure. Maybe I should have prettier or something.

--- services/userServices/userUpdateService.js
+++ services/userServices/userUpdateService.js
@@ -2,9 +2,10 @@ const userModel = require('../../models/userModel.js')
 
 const userUpdateService = async (userId, userBody) => {
     try {
-         return await userModel.findByIdAndUpdate(userId, {
-             ...userBody
-         })
+         return await userModel.findByIdAndUpdate(userId,
+             {
+                 ...userBody
+             })
     } catch (error) {
         throw Error("Error while updating user. Location: userUpdateService")
     }
