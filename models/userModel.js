7d376e7f702b65380ffc4853f494eeab79e5791e
Isle <90284341+ixiiviixi@users.noreply.github.com>
2022-11-23 09:18:36 -0800

    sessionKey + format
    changed sessionKey from an empty string to an array.

--- models/userModel.js
+++ models/userModel.js
@@ -2,42 +2,42 @@
 
 const mongoose = require( 'mongoose' )
 
-const userSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true
-        },
-        email: {
-            type: String,
-            required: true
-        },
-        password: {
-            type: String,
-            required: true
-        },
-        sessionKey: {
+const userSchema = new mongoose.Schema( {
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    sessionKey: [
+        {
             type: String,
             required: false
-        },
-        userForums: [
-            {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'Forum'
-            }
-        ],
-        userComments: [
-            {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'Comment'
-            }
-        ],
-        createdOn: {
-            type: Date,
-            default: Date.now
         }
+    ],
+    userForums: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Forum'
+        }
+    ],
+    userComments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ],
+    createdOn: {
+        type: Date,
+        default: Date.now
     }
-)
+} )
 
 const User = mongoose.model( 'User', userSchema )
 module.exports = User
