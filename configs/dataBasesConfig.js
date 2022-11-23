abb294fa40588139705fef5300252e8198e82e98
Isle <90284341+ixiiviixi@users.noreply.github.com>
2022-11-23 09:15:31 -0800

    config cleanup

--- configs/dataBasesConfig.js
+++ configs/dataBasesConfig.js
@@ -1,22 +1,22 @@
-require('dotenv').config()
+require( 'dotenv' ).config()
 
 const dataBasesConfig = {
     //MongoDB Database Config
-
-    MONGODB_URI: `mongodb+srv://${process.env.MONGO_USERNAME}:${encodeURIComponent(process.env.MONGO_PASSWORD)}@${process.env.MONGO_HOST}/${process.env.MONGO_DATABASE}?${process.env.MONGO_OPTION}`,
-
+    
+    MONGODB_URI: `mongodb+srv://${ process.env.MONGO_USERNAME }:${ encodeURIComponent( process.env.MONGO_PASSWORD ) }@${ process.env.MONGO_HOST }/${ process.env.MONGO_DATABASE }?${ process.env.MONGO_OPTION }`,
+    
     MONGODB_OPTIONS: {
         useNewUrlParser: true,
-        useUnifiedTopology: true,
+        useUnifiedTopology: true
     },
-
+    
     //Redis Database Config
     // cache \\
     REDIS_OPTIONS: {
         port: +process.env.REDIS_PORT,
         host: process.env.REDIS_HOST,
         password: process.env.REDIS_PASSWORD
-    },
+    }
 }
 module.exports = {
     dataBasesConfig
--- configs/sessionConfig.js
+++ configs/sessionConfig.js
@@ -1,25 +1,27 @@
-require('dotenv').config()
-const {nanoid} = require('nanoid')
-const IN_PROD = process.env.NODE_ENV === 'production'
+require( 'dotenv').config()
+
+const { nanoid } = require( 'nanoid' )
+const IN_PRODUCTION = process.env.NODE_ENV === 'production'
 
 const sessionConfig = {
 
 // session \\
     SESSION_OPTIONS: {
         secret: process.env.SESSION_SECRET,
-        genid: (req) => {
+        genid: ( req ) => {
             return nanoid()
         },
         name: process.env.SESSION_NAME,
         cookie: {
             maxAge: +process.env.SESSION_IDLE_TIMEOUT,
-            secure: IN_PROD, //if true then transmit cookie over https
-            httpOnly: IN_PROD, //if true: prevents client side JS from reading the cookie
-            sameSite: true,
+            secure: IN_PRODUCTION, //if true then transmit cookie over https
+            httpOnly: IN_PRODUCTION, //if true: prevents client side JS from reading the cookie
+            sameSite: true
         },
         rolling: true,
         resave: false, //if call is made and nothing is added, then we will not override session
-        saveUninitialized: false, //if you make a request to the server, and no data is stored to the session then it will not be written to the database.
+        saveUninitialized: false //if you make a request to the server, and no data is stored to
+        // the session then it will not be written to the database.
     }
 }
 module.exports = {
