e8f5b6ee76c47319ca4d60c16392191ddcd6559b
Isle <90284341+ixiiviixi@users.noreply.github.com>
2022-11-23 09:16:52 -0800

    small grammar fixes

--- middlewares/sessionManagement/redisSession.js
+++ middlewares/sessionManagement/redisSession.js
@@ -1,13 +1,13 @@
-const { sessionConfig } = require ( '../../configs/sessionConfig' )
-const { dataBasesConfig } = require ( '../../configs/dataBasesConfig' )
-const Redis = require ( 'ioredis' )
-const session = require ( 'express-session' )
-const RedisStore = require ( 'connect-redis' ) ( session )
+const { sessionConfig } = require( '../../configs/sessionConfig' )
+const { dataBasesConfig } = require( '../../configs/dataBasesConfig' )
+const Redis = require( 'ioredis' )
+const session = require( 'express-session' )
+const RedisStore = require( 'connect-redis' )( session )
 
-const client = new Redis ( dataBasesConfig.REDIS_OPTIONS )
+const client = new Redis( dataBasesConfig.REDIS_OPTIONS )
 
-const test = client.ping ( function ( error, result ) {
-    console.log ( 'ping', result )
+const test = client.ping( function( error, result ) {
+    console.log( 'ping', result )
 } )
 //TODO look into making the session cookie safer by:
 //Using length and random session ID to prevent brute force attack. The recc length is 128 bits
@@ -15,13 +15,13 @@ const test = client.ping ( function ( error, result ) {
 // meaning HTTPS communication only. No http. Secure and HTTP only cookies. All session cookies
 // should be created with secure and HTTP only attributes Manage sessions. Destroy sessions upon
 // closing browser, timeout, logout, or log-in from a separate location.
-const redisSession = session ( {
+const redisSession = session( {
     ...sessionConfig.SESSION_OPTIONS,
-    store: new RedisStore ( {
-        client,
-    } ),
+    store: new RedisStore( {
+        client
+    } )
 } )
 
 module.exports = {
-    redisSession,
+    redisSession
 }
